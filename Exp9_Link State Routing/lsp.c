d1769c92c0f747280c67932d2770b26c1cdf3111
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-02-24 19:25:42 +0530

    lsp

--- Exp9_Link State Routing/lsp.c
+++ Exp9_Link State Routing/lsp.c
@@ -84,18 +84,59 @@ int main()
 		}
 	}while(count != 0);
 
-	printf("Destination Distance NextRouter\n");
+	printf("Minimum Distance from %d to all the other routers:\n", src);
+	printf("Destination Distance\n");
 	for(i = 0; i < n; i++)
 	{
-		printf("%11d %8d %10d\n", i, tab[src][i].dist, tab[src][i].next);
+		printf("%11d %8d\n", i, tab[src][i].dist);
 	}
 
-	printf("Cost Matrix:\n");
-	for(i = 0; i < n; i++)
-	{
-		for(j = 0; j < n; j++)
-			printf("%d ",costs[i][j]);
-		printf("\n");
-	}
+	printf("Cost Matrix:\n      ");
+        for(i = 0; i < n; i++)
+                printf("%3d ",i);
+        printf("\n------------------------------------------\n");
+
+        for(i = 0; i < n; i++)
+        {
+                printf("%3d | ",i);
+                for(j = 0; j < n; j++)
+                        printf("%3d ",costs[i][j]);
+                printf("\n");
+        }
 }
 
+/*
+Sample Input:
+7
+
+0 1 1 -1 1 1 -1
+1 0 1 -1 -1 -1 -1
+1 1 0 1 -1 -1 -1
+-1 -1 1 0 -1 -1 1
+1 -1 -1 -1 0 -1 -1
+1 -1 -1 -1 -1 0 1
+-1 -1 -1 1 -1 1 0
+
+2
+
+Sample Output:
+Minimum Distance from 2 to all the other routers:
+Destination Distance
+          0        1
+          1        1
+          2        0
+          3        1
+          4        2
+          5        2
+          6        2
+Cost Matrix:
+        0   1   2   3   4   5   6
+------------------------------------------
+  0 |   0   1   1   2   1   1   2
+  1 |   1   0   1   2   2   2   3
+  2 |   1   1   0   1   2   2   2
+  3 |   2   2   1   0   3   2   1
+  4 |   1   2   2   3   0   2   3
+  5 |   1   2   2   2   2   0   1
+  6 |   2   3   2   1   3   1   0
+*/
