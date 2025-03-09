8f04d053a1210f9dda31848cd5296891e95a3a73
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-03-09 23:17:31 +0530

    tcp application

--- Exp4_TCP Capitalize/client.c
+++ Exp4_TCP Capitalize/client.c
@@ -1,68 +1,70 @@
+// header files
 #include <stdio.h>
 #include <stdlib.h>
-#include <netdb.h>
-#include <netinet/in.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#define MAX 100
-#define PORT 3014
-#define SA struct sockaddr
+#include <sys/socket.h>
 
-void func(int sockfd)
+// definitions
+#define BUFFER_SIZE 1024
+
+// global declarations
+int client_socket;
+char buffer[BUFFER_SIZE];
+
+// main
+int main(int argc, char *argv[])
 {
-	char buff[MAX];
-	int n;
-	while(1)
-	{
-		bzero(buff,MAX);
-		printf("Enter a string: ");
-		n = 0;
-		while((buff[n++] = getchar()) != '\n');
-		write(sockfd, buff, sizeof(buff));
-		if(strncmp("exit",buff,4) == 0)
-		{
-			printf("Client exits\n");
-			break;
-		}
-		bzero(buff,sizeof(buff));
-		read(sockfd,buff,sizeof(buff));
-		printf("From Server: %s\n",buff);
-	}
+    int port = atoi(argv[1]);
+
+    // address structures
+    struct sockaddr_in server_addr;
+
+    // socket creation
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1)
+    {
+        perror("Socket creation failed");
+        exit(1);
+    }
+
+    // structure set up
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+
+    // connect to server
+    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+    {
+        perror("Connection Failed...");
+        exit(1);
+    }
+    printf("Connection Successful\n");
+
+    while (1)
+    {
+        // Send message
+        fgets(buffer, BUFFER_SIZE, stdin);
+        send(client_socket, buffer, strlen(buffer), 0);
+
+        // Exit condition
+        if (strncmp(buffer, "exit", 4) == 0)
+        {
+            printf("Client exiting\n");
+            break;
+        }
+
+        // Receive message
+        memset(buffer, 0, BUFFER_SIZE);
+        int bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
+        if (bytes_received <= 0)
+        {
+            printf("Server disconnected.\n");
+            break;
+        }
+        printf("Server: %s", buffer);
+    }
+
+    close(client_socket);
 }
-
-int main()
-{
-	int sockfd, connfd;
-	struct sockaddr_in servaddr, cli;
-	sockfd = socket(AF_INET, SOCK_STREAM,0);
-	
-	if(sockfd == -1)
-	{
-		printf("Socket creation failed\n");
-		exit(0);
-	}
-	else
-	{
-		printf("Socket creation successful\n");
-		bzero(&servaddr, sizeof(servaddr));
-		servaddr.sin_family = AF_INET;
-		servaddr.sin_addr.s_addr = inet_addr("127.0.0.20");
-		servaddr.sin_port = htons(PORT);
-		if(connect(sockfd,(SA*)&servaddr, sizeof(servaddr)) != 0)
-		{
-			printf("Connection failed\n");
-			exit(0);
-		}
-		else
-		{
-			printf("Connection successful\n");
-			func(sockfd);
-			close(connfd);
-		}
-
-	}
-}
-
