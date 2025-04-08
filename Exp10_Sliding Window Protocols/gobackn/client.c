fa719b8980b09dc0f03db7237746b9e336b7fedb
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-04-08 15:33:52 +0530

    go back n

--- Exp10_Sliding Window Protocols/gobackn/client.c
+++ Exp10_Sliding Window Protocols/gobackn/client.c
@@ -1,72 +1,129 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/select.h>
 #include <time.h>
 
-#define WINDOW_SIZE 4
-#define TOTAL_PACKETS 10
+#define BUFFER_SIZE 20
+#define WINDOW_SIZE 5
+#define FRAMES 7
 
 int main(int argc, char *argv[])
 {
-    int client_socket;
-    struct sockaddr_in server_addr;
-    int base = 0, next_seq = 0, ack, i;
+        int client_socket;
 
-    if (argc < 2)
-    {
-        printf("Usage: %s <port>\n", argv[0]);
-        exit(1);
-    }
+        struct sockaddr_in server_addr, client_addr;
+        socklen_t server_len = sizeof(server_addr);
 
-    client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket == -1)
-    {
-        perror("Socket creation failed");
-        exit(1);
-    }
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(atoi(argv[1]));
-    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
-
-    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
-    {
-        perror("Connection failed");
-        exit(1);
-    }
-    printf("Connected to server.\n");
-
-    srand(time(NULL));
-
-    while (base < TOTAL_PACKETS)
-    {
-        for (i = 0; i < WINDOW_SIZE && (base + i) < TOTAL_PACKETS; i++)
+        if((client_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
         {
-            if (rand() % 4 != 0) // Randomly drop packets (1 in 4 chance)
-            {
-                next_seq = base + i;
-                send(client_socket, &next_seq, sizeof(next_seq), 0);
-                printf("Sent seq: %d\n", next_seq);
-            }
-            else
-            {
-                printf("Packet seq %d lost! Will resend on timeout.\n", base + i);
-            }
+                perror("Socket creation failed");
+                exit(1);
+        }
+        printf("Socket creation successful\n");
+
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(atoi(argv[1]));
+        inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+
+        if((connect(client_socket, (struct sockaddr*)&server_addr, server_len)) == -1)
+        {
+                perror("Connection failed");
+                exit(1);
+        }
+        printf("Connection successful\n");
+
+        int next_seq = 0, ack, base = 0;
+        char buffer[BUFFER_SIZE];
+        int acked[FRAMES], i;
+        for(i = 0; i<FRAMES; i++)
+                acked[i] = 0;
+
+        struct timeval timeout;
+        fd_set readfds;
+        srand(time(0));
+
+        while(base < FRAMES)
+        {
+                while(next_seq <= base+WINDOW_SIZE-1 && next_seq < FRAMES)
+                {
+                        // frame loss
+                        if(rand() % 100 < 20)
+                        {
+                                printf("Simulating frame lost for seq %d\n", next_seq);
+                                next_seq++;
+                                //continue;
+                        }
+                        else
+                        {
+                                // sending frame
+//                              memset(buffer, 0, BUFFER_SIZE);
+//                              sprintf(buffer, "%d", next_seq);
+                                send(client_socket, &next_seq, sizeof(int), 0);
+                                printf("Sending frame %d\n", next_seq);
+                                next_seq++;
+                        }
+                        /*
+                        FD_ZERO(&readfds);
+                        FD_SET(client_socket, &readfds);
+                        timeout.tv_sec = 3;
+                        timeout.tv_usec = 0;
+
+                        */
+                }
+                FD_ZERO(&readfds);
+                FD_SET(client_socket, &readfds);
+                timeout.tv_sec = 3;
+                timeout.tv_usec = 0;
+
+                if(select(client_socket + 1, &readfds, NULL, NULL, &timeout) == 0)
+                {
+                        printf("Timeout..., Resend from frame %d\n", base);
+                        next_seq = base;
+                }
+                else
+                {
+                        //memset(buffer, 0, BUFFER_SIZE);
+                        if((recv(client_socket, &ack, sizeof(int), 0)) > 0)
+                        {
+                                //ack = atoi(buffer);
+                                printf("Received ACK %d\n", ack);
+                                if(ack > 0)
+                                        acked[ack-1] = 1;
+                                if(ack > base)
+                                        base = ack;
+                        }
+                }
+                for(i = 0; i < FRAMES; i++)
+                        if(acked[i] == 0)
+                                break;
+                base = i;
         }
 
-        if (recv(client_socket, &ack, sizeof(ack), 0) > 0)
-        {
-            printf("Received ack: %d\n", ack);
-            base = ack;
-        }
-        else
-        {
-            printf("Timeout! Resending window starting from seq: %d\n", base);
-        }
-    }
+        close(client_socket);
+        return 0;
 
-    close(client_socket);
-
-    return 0;
 }
+
+
+/*
+
+Socket creation successful
+Connection successful
+Sending frame 0
+Sending frame 1
+Sending frame 2
+Sending frame 3
+Sending frame 4
+Received ACK 1
+Sending frame 5
+Received ACK 2
+Sending frame 6
+Received ACK 3
+Received ACK 5
+Received ACK 6
+Received ACK 7
+*/
--- Exp10_Sliding Window Protocols/gobackn/server.c
+++ Exp10_Sliding Window Protocols/gobackn/server.c
@@ -1,83 +1,119 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/time.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <time.h>
 
-#define WINDOW_SIZE 4
-#define TOTAL_PACKETS 10
+#define BUFFER_SIZE 20
+#define FRAMES 7
 
 int main(int argc, char *argv[])
 {
-    int server_socket, client_socket;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_len = sizeof(client_addr);
-    int seq, expected_seq = 0, ack;
-    struct timeval timeout = {3, 0}; // 3-second timeout
+        int server_socket, client_socket;
 
-    if (argc < 2)
-    {
-        printf("Usage: %s <port>\n", argv[0]);
-        exit(1);
-    }
+        struct sockaddr_in server_addr, client_addr;
+        socklen_t client_len = sizeof(client_addr);
 
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1)
-    {
-        perror("Socket creation failed");
-        exit(1);
-    }
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(atoi(argv[1]));
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-
-    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
-    {
-        perror("Binding failed");
-        exit(1);
-    }
-
-    if (listen(server_socket, 5) == -1)
-    {
-        perror("Listening failed");
-        exit(1);
-    }
-    printf("Server is listening...\n");
-
-    client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_len);
-    if (client_socket == -1)
-    {
-        perror("Accept failed");
-        exit(1);
-    }
-    printf("Client connected.\n");
-
-    setsockopt(client_socket, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
-
-    while (expected_seq < TOTAL_PACKETS)
-    {
-        if (recv(client_socket, &seq, sizeof(seq), 0) > 0)
+        if((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
         {
-            printf("Received seq: %d\n", seq);
-
-            if (seq == expected_seq)
-            {
-                expected_seq++;
-            }
-
-            ack = expected_seq;
-            send(client_socket, &ack, sizeof(ack), 0);
-            printf("Sent ack: %d\n", ack);
+                perror("Socket creation failed");
+                exit(1);
         }
-        else
+        printf("Socket creation successful\n");
+
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(atoi(argv[1]));
+        server_addr.sin_addr.s_addr = INADDR_ANY;
+
+        if(bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
         {
-            printf("Timeout! No packet received. Waiting...\n");
+                perror("Socket binding failed");
+                exit(1);
         }
-    }
+        printf("Socket binding successful\n");
 
-    close(client_socket);
-    close(server_socket);
+        if(listen(server_socket, 5) == -1)
+        {
+                perror("Listening failed");
+                exit(1);
+        }
+        printf("Server listening\n");
 
-    return 0;
+        if((client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_len)) == -1)
+        {
+                perror("Accepting failed");
+                exit(1);
+        }
+        printf("Connected to Client\n");
+
+        int ack, expected_seq = 0, rec_seq;
+        char buffer[BUFFER_SIZE];
+
+        while(expected_seq < FRAMES)
+        {
+                memset(buffer, 0, BUFFER_SIZE);
+                if(recv(client_socket, &rec_seq, sizeof(int), 0) > 0)
+                {
+                        //rec_seq = atoi(buffer);
+                        printf("Received frame %d\n", rec_seq);
+                        if(rec_seq < expected_seq)
+                        {
+                                printf("Duplicate frame received. Expecting frame %d\n", expected_seq);
+                                send(client_socket, &expected_seq, sizeof(int), 0);
+                                printf("Sent ACK %d\n", expected_seq);
+                        }
+                        else if(rec_seq > expected_seq)
+                        {
+                                printf("Out-of-Order frame received. Still expecting frame %d\n", expected_seq);
+                                send(client_socket, &expected_seq, sizeof(int), 0);
+                                printf("Sent ACK %d\n", expected_seq);
+                        }
+                        else
+                        {
+                                // simulating ack lost
+                                if(rand() % 100 < 20)
+                                {
+                                        printf("Simulating ACK lost for ACK %d\n", rec_seq +1);
+                                        expected_seq = rec_seq + 1;
+                                }
+                                else
+                                {
+                                        ack = rec_seq + 1;
+                                        send(client_socket, &ack, sizeof(int),0);
+                                        printf("Sent ACK %d\n", ack);
+                                        expected_seq = ack;
+
+                                }
+                        }
+                }
+        }
+
+        close(client_socket);
+        close(server_socket);
+
+        return 0;
 }
+
+/*
+Socket creation successful
+Socket binding successful
+Server listening
+Connected to Client
+Received frame 0
+Sent ACK 1
+Received frame 1
+Sent ACK 2
+Received frame 2
+Sent ACK 3
+Received frame 3
+Simulating ACK lost for ACK 4
+Received frame 4
+Sent ACK 5
+Received frame 5
+Sent ACK 6
+Received frame 6
+Sent ACK 7
+*/
